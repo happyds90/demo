fee918cf663cd94c70dd310a39933357eec97382
happyds90 <38491590+happyds90@users.noreply.github.com>
2018-10-11 12:47:47 +0800

    添加controller

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Author: hzj
+ * Date: 2018-10-11 12:47
+ */
+@Controller
+public class DemoController {
+    
+}
